5c3887871672f7d1c3d538650d06d741e66e9dd5
= <aameurnouhaila@gmail.com>
2023-07-14 11:08:06 +0100

    security

---
+++ elk/src/main/java/com/projectkafka/controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.projectkafka.controller;
+
+import com.projectkafka.config.JwtUtils;
+import com.projectkafka.dao.UserDao;
+import com.projectkafka.dto.AuthenticationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDao userDao;
+    private final JwtUtils jwtUtils;
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(
+            @RequestBody AuthenticationRequest request
+    ){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        final UserDetails user = userDao.findUserByEmail(request.getEmail());
+        if (user != null) {
+            return ResponseEntity.ok(jwtUtils.generateToken(user));
+        }
+        return ResponseEntity.status(400).body("some errors has occured");
+    }
+}
